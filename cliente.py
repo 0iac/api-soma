24c463457122ee387e7fb9f42fbfc64e1dd94160
0iac <mybelovedcocoa@gmail.com>
2026-06-19 22:33:03 -0300

    H2: cliente Python consumindo a API via GET

---
+++ cliente.py
@@ -0,0 +1,11 @@
+import requests
+
+URL = "http://127.0.0.1:5000/api/soma"
+
+parametros = {"a": 2, "b": 3}
+
+resposta = requests.get(URL, params=parametros)
+
+print("Status:", resposta.status_code) # 200 = OK
+print("Cabecalhos:", dict(resposta.headers)) # metadados da resposta
+print("Corpo (JSON):", resposta.json()) # {’resultado’: 5.0}# Em GET, os parametros vao na "query string": ?a=2&b=3
